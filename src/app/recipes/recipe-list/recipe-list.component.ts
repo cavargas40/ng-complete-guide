fe2ae84cc8c38810c1faa62bfad9e871923a656c
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-07 11:18:49 -0500

    Recipe List, Recipe Detail and tsconfig baseUrl

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'app/recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  public recipes: Array<Recipe> = [
+    new Recipe({
+      name: 'A Test Recipe',
+      description: 'This is simply a test',
+      imagePath: 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1000,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
+    }),
+  ];
 
   constructor() { }
 

de76ea38d61d06220bb27d7be420846a40ea5986
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-08 12:08:15 -0500

    recipe section working properly for navigation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from 'app/recipes/recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from 'app/recipes/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   
   public recipes: Array<Recipe> = [
     new Recipe({
@@ -14,11 +16,21 @@ export class RecipeListComponent implements OnInit {
       description: 'This is simply a test',
       imagePath: 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1000,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
     }),
+    new Recipe({
+      name: 'Another Test Recipe',
+      description: 'This is simply a test',
+      imagePath: 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1000,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
+    }),
+
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
   }
 
 }

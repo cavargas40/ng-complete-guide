9e6b62eb5bc2cdb64abd0e67120768c4b288d68a
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-13 21:55:24 -0500

    auth error handling in the service (400, "This email already exists")

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 interface AuthResponseData {
   kind: string;
@@ -19,13 +21,25 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.httpClient.post<AuthResponseData>(
-      `${this.authUrl}?key=${this.apiKey}`,
-      {
+    return this.httpClient
+      .post<AuthResponseData>(`${this.authUrl}?key=${this.apiKey}`, {
         email,
         password,
         returnSecureToken: true
-      }
-    );
+      })
+      .pipe(
+        catchError(errorRes => {
+          let errorMessage = "An unknown error ocurred!";
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "This email already exists";
+              break;
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
